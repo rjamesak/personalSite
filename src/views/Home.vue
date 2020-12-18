c3b5d0055edcb06818aaf5bc9bd6d0b2a1b33b11
Russell James <jamesru@oregonstate.edu>
2020-12-17 17:02:22 -0900

    home vue, trying out parallax images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="parallax"></div>
+
+    <div class="card">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa turpis, dignissim id feugiat eget, fermentum quis diam. Ut eleifend aliquam enim nec lobortis. Vivamus iaculis, neque volutpat efficitur imperdiet, lorem ligula aliquet tellus, ut iaculis eros urna eget diam. Donec maximus orci ut odio pretium, id interdum velit ullamcorper. In ut turpis quis est vehicula pellentesque. Donec ornare augue egestas, lacinia eros et, tincidunt augue. Curabitur id sagittis quam. Nam cursus nunc non felis sodales luctus. Quisque bibendum convallis massa, vitae ullamcorper risus ultrices ut.
+
+Donec mi felis, semper eget viverra nec, consequat non arcu. Duis sit amet cursus quam, id elementum ex. In id consequat sem. Aenean ullamcorper eleifend lacus, ac congue elit. Duis sodales dignissim venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Praesent laoreet et lorem id iaculis. Duis blandit consectetur lacus, id pellentesque odio laoreet at. Aenean quis tristique orci. Vestibulum in ipsum metus. Vivamus dapibus ut urna vel feugiat. Nulla lobortis mi et turpis dictum rutrum. Proin in arcu arcu.
+
+Proin est mauris, consequat a egestas ac, luctus non tellus. Sed dapibus, est eget laoreet maximus, nulla metus volutpat est, et hendrerit odio nisi tempus tellus. Pellentesque sit amet commodo turpis. Maecenas blandit massa sed neque malesuada vulputate sed sed mauris. Aliquam lacinia, lectus sed tempus accumsan, purus diam lobortis turpis, sit amet maximus ex risus quis sapien. Vestibulum mattis elit ut aliquet tempus. Pellentesque ac elit pulvinar, interdum ipsum et, vulputate neque. Nullam tempus, lorem ac ullamcorper ultrices, ante urna laoreet est, et maximus metus quam a lectus. Quisque interdum mauris in mauris iaculis, semper vehicula metus bibendum. Praesent sem velit, pellentesque vel leo in, lobortis aliquam diam. Nulla interdum ullamcorper turpis, et porttitor purus eleifend ut. Maecenas molestie sodales efficitur. Nam pulvinar ex et ante sodales ullamcorper.
+
+Ut eu semper diam, convallis consectetur nulla. Donec vehicula consectetur ante at lobortis. Vestibulum aliquam mi varius est commodo pellentesque. Vivamus porta augue turpis, non venenatis metus gravida eget. Suspendisse finibus vehicula porta. Mauris vestibulum vel orci id faucibus. Nullam vel lorem sit amet odio lobortis vulputate. Curabitur sit amet molestie odio. Nulla eu arcu eu lacus finibus pellentesque. Suspendisse potenti. Donec malesuada nulla quis sapien laoreet dapibus. Fusce a nulla ac quam aliquam blandit.
+
+Vestibulum mattis orci non mi venenatis, vitae cursus augue egestas. Cras in neque risus. Donec quis egestas lectus. Pellentesque vitae semper elit, id ultricies nunc. Pellentesque vestibulum dignissim rutrum. Nulla erat leo, pulvinar vel leo in, posuere volutpat neque. Fusce non tempus ligula. Vivamus pharetra, lorem vel sollicitudin sagittis, lacus purus semper nisl, laoreet porta ante magna at lacus. Maecenas id maximus est. Nullam sed tortor nibh. Pellentesque consectetur nunc et massa placerat, vel aliquet nisi iaculis. Duis sollicitudin nisl quis nunc pulvinar viverra. Quisque blandit erat ut tortor pellentesque consectetur. Ut non felis purus. Fusce id ipsum mauris.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa turpis, dignissim id feugiat eget, fermentum quis diam. Ut eleifend aliquam enim nec lobortis. Vivamus iaculis, neque volutpat efficitur imperdiet, lorem ligula aliquet tellus, ut iaculis eros urna eget diam. Donec maximus orci ut odio pretium, id interdum velit ullamcorper. In ut turpis quis est vehicula pellentesque. Donec ornare augue egestas, lacinia eros et, tincidunt augue. Curabitur id sagittis quam. Nam cursus nunc non felis sodales luctus. Quisque bibendum convallis massa, vitae ullamcorper risus ultrices ut.
+
+Donec mi felis, semper eget viverra nec, consequat non arcu. Duis sit amet cursus quam, id elementum ex. In id consequat sem. Aenean ullamcorper eleifend lacus, ac congue elit. Duis sodales dignissim venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Praesent laoreet et lorem id iaculis. Duis blandit consectetur lacus, id pellentesque odio laoreet at. Aenean quis tristique orci. Vestibulum in ipsum metus. Vivamus dapibus ut urna vel feugiat. Nulla lobortis mi et turpis dictum rutrum. Proin in arcu arcu.
+
+Proin est mauris, consequat a egestas ac, luctus non tellus. Sed dapibus, est eget laoreet maximus, nulla metus volutpat est, et hendrerit odio nisi tempus tellus. Pellentesque sit amet commodo turpis. Maecenas blandit massa sed neque malesuada vulputate sed sed mauris. Aliquam lacinia, lectus sed tempus accumsan, purus diam lobortis turpis, sit amet maximus ex risus quis sapien. Vestibulum mattis elit ut aliquet tempus. Pellentesque ac elit pulvinar, interdum ipsum et, vulputate neque. Nullam tempus, lorem ac ullamcorper ultrices, ante urna laoreet est, et maximus metus quam a lectus. Quisque interdum mauris in mauris iaculis, semper vehicula metus bibendum. Praesent sem velit, pellentesque vel leo in, lobortis aliquam diam. Nulla interdum ullamcorper turpis, et porttitor purus eleifend ut. Maecenas molestie sodales efficitur. Nam pulvinar ex et ante sodales ullamcorper.
+
+Ut eu semper diam, convallis consectetur nulla. Donec vehicula consectetur ante at lobortis. Vestibulum aliquam mi varius est commodo pellentesque. Vivamus porta augue turpis, non venenatis metus gravida eget. Suspendisse finibus vehicula porta. Mauris vestibulum vel orci id faucibus. Nullam vel lorem sit amet odio lobortis vulputate. Curabitur sit amet molestie odio. Nulla eu arcu eu lacus finibus pellentesque. Suspendisse potenti. Donec malesuada nulla quis sapien laoreet dapibus. Fusce a nulla ac quam aliquam blandit.
+
+    </div>
+
+    <div class="parallax2">
+    </div>
+
+
+    <div class="text2">
+Vestibulum mattis orci non mi venenatis, vitae cursus augue egestas. Cras in neque risus. Donec quis egestas lectus. Pellentesque vitae semper elit, id ultricies nunc. Pellentesque vestibulum dignissim rutrum. Nulla erat leo, pulvinar vel leo in, posuere volutpat neque. Fusce non tempus ligula. Vivamus pharetra, lorem vel sollicitudin sagittis, lacus purus semper nisl, laoreet porta ante magna at lacus. Maecenas id maximus est. Nullam sed tortor nibh. Pellentesque consectetur nunc et massa placerat, vel aliquet nisi iaculis. Duis sollicitudin nisl quis nunc pulvinar viverra. Quisque blandit erat ut tortor pellentesque consectetur. Ut non felis purus. Fusce id ipsum mauris.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa turpis, dignissim id feugiat eget, fermentum quis diam. Ut eleifend aliquam enim nec lobortis. Vivamus iaculis, neque volutpat efficitur imperdiet, lorem ligula aliquet tellus, ut iaculis eros urna eget diam. Donec maximus orci ut odio pretium, id interdum velit ullamcorper. In ut turpis quis est vehicula pellentesque. Donec ornare augue egestas, lacinia eros et, tincidunt augue. Curabitur id sagittis quam. Nam cursus nunc non felis sodales luctus. Quisque bibendum convallis massa, vitae ullamcorper risus ultrices ut.
+
+Donec mi felis, semper eget viverra nec, consequat non arcu. Duis sit amet cursus quam, id elementum ex. In id consequat sem. Aenean ullamcorper eleifend lacus, ac congue elit. Duis sodales dignissim venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Praesent laoreet et lorem id iaculis. Duis blandit consectetur lacus, id pellentesque odio laoreet at. Aenean quis tristique orci. Vestibulum in ipsum metus. Vivamus dapibus ut urna vel feugiat. Nulla lobortis mi et turpis dictum rutrum. Proin in arcu arcu.
+
+Proin est mauris, consequat a egestas ac, luctus non tellus. Sed dapibus, est eget laoreet maximus, nulla metus volutpat est, et hendrerit odio nisi tempus tellus. Pellentesque sit amet commodo turpis. Maecenas blandit massa sed neque malesuada vulputate sed sed mauris. Aliquam lacinia, lectus sed tempus accumsan, purus diam lobortis turpis, sit amet maximus ex risus quis sapien. Vestibulum mattis elit ut aliquet tempus. Pellentesque ac elit pulvinar, interdum ipsum et, vulputate neque. Nullam tempus, lorem ac ullamcorper ultrices, ante urna laoreet est, et maximus metus quam a lectus. Quisque interdum mauris in mauris iaculis, semper vehicula metus bibendum. Praesent sem velit, pellentesque vel leo in, lobortis aliquam diam. Nulla interdum ullamcorper turpis, et porttitor purus eleifend ut. Maecenas molestie sodales efficitur. Nam pulvinar ex et ante sodales ullamcorper.
+
+Ut eu semper diam, convallis consectetur nulla. Donec vehicula consectetur ante at lobortis. Vestibulum aliquam mi varius est commodo pellentesque. Vivamus porta augue turpis, non venenatis metus gravida eget. Suspendisse finibus vehicula porta. Mauris vestibulum vel orci id faucibus. Nullam vel lorem sit amet odio lobortis vulputate. Curabitur sit amet molestie odio. Nulla eu arcu eu lacus finibus pellentesque. Suspendisse potenti. Donec malesuada nulla quis sapien laoreet dapibus. Fusce a nulla ac quam aliquam blandit.
+
+Vestibulum mattis orci non mi venenatis, vitae cursus augue egestas. Cras in neque risus. Donec quis egestas lectus. Pellentesque vitae semper elit, id ultricies nunc. Pellentesque vestibulum dignissim rutrum. Nulla erat leo, pulvinar vel leo in, posuere volutpat neque. Fusce non tempus ligula. Vivamus pharetra, lorem vel sollicitudin sagittis, lacus purus semper nisl, laoreet porta ante magna at lacus. Maecenas id maximus est. Nullam sed tortor nibh. Pellentesque consectetur nunc et massa placerat, vel aliquet nisi iaculis. Duis sollicitudin nisl quis nunc pulvinar viverra. Quisque blandit erat ut tortor pellentesque consectetur. Ut non felis purus. Fusce id ipsum mauris.
+
+
+    </div>
+
+    </div> 
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
+
+<style scoped>
+.parallax {
+  background-image: url("http://localhost:8000/pexels-howard-herdi-5806710.jpg");
+  height: 400px;
+  /* parallax effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.parallax2 {
+  background-image: url("http://localhost:8000/hari-nandakumar-kQG1XssK3Os-unsplash.jpg");
+  height: 600px;
+  /* parallax effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.card {
+  /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); */
+  /* border-radius: 5px; */
+  padding: 20px;
+  margin: 10px;
+}
+.text2 {
+  background: none;
+
+}
+</style>
